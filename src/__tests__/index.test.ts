25f5d874db10fac42c442d6206fcbcad4abe2b80
FoggDev <foggdev@gmail.com>
2023-09-30 21:30:11 -0600

    Changing base64 to security util

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,46 +1,154 @@
 import { describe, expect, it } from '@jest/globals'
 
-import base64 from '../index'
+import security from '../index'
 
-describe('base64', () => {
-  describe('get', () => {
+describe('security', () => {
+  describe('string', () => {
+    it('generates a random code of specified length', () => {
+      expect(security.string.code(10).length).toBe(10)
+      expect(security.string.code(5).length).toBe(5)
+    })
+
+    it('sanitizes strings', () => {
+      expect(security.string.sanitize('<script>alert("test")</script>')).toBe(
+        '&lt;script&gt;alert(&quot;test&quot;)&lt;/script&gt;'
+      )
+    })
+  })
+
+  describe('base64', () => {
+    it('encodes and decodes correctly', () => {
+      const original = { test: 'value' }
+      const encoded = security.base64.encode(original)
+      const decoded = security.base64.decode(encoded)
+      expect(decoded).toEqual(original)
+    })
+
+    it('should encode a string to base64', () => {
+      const str = 'Hello World'
+      const result = security.base64.encode(str)
+      const expected = Buffer.from(str).toString('base64')
+      expect(result).toBe(expected)
+    })
+
+    it('should return null for non-string, non-object values', () => {
+      expect(security.base64.encode(12345)).toBeNull()
+      expect(security.base64.encode(true)).toBeNull()
+    })
+
     it('should decode a base64 encoded string', () => {
       const encoded = Buffer.from('Hello World').toString('base64')
-      const result = base64.get(encoded)
+      const result = security.base64.decode(encoded)
       expect(result).toBe('Hello World')
     })
 
     it('should decode and parse a base64 encoded JSON string', () => {
       const obj = { key: 'value' }
       const encoded = Buffer.from(JSON.stringify(obj)).toString('base64')
-      const result = base64.get(encoded)
+      const result = security.base64.decode(encoded)
       expect(result).toEqual(obj)
     })
 
     it('should return an empty string for non-string values', () => {
-      expect(base64.get(12345)).toBe('')
-      expect(base64.get(true)).toBe('')
+      expect(security.base64.decode(12345)).toBe('')
+      expect(security.base64.decode(true)).toBe('')
     })
   })
 
-  describe('set', () => {
-    it('should encode a string to base64', () => {
-      const str = 'Hello World'
-      const result = base64.set(str)
-      const expected = Buffer.from(str).toString('base64')
-      expect(result).toBe(expected)
+  describe('password', () => {
+    it('should validate a strong password', () => {
+      const result = security.password.validation('StrongPass123!')
+      expect(result.isValid).toBe(true)
+      expect(result.reasons?.length).toBe(0)
     })
 
-    it('should stringify and encode an object to base64', () => {
-      const obj = { key: 'value' }
-      const result = base64.set(obj)
-      const expected = Buffer.from(JSON.stringify(obj)).toString('base64')
-      expect(result).toBe(expected)
+    it('should invalidate a short password', () => {
+      const result = security.password.validation('Shrt1!')
+      expect(result.isValid).toBe(false)
+      expect(result.length).toBe('Password should be at least 8 characters long.')
+      expect(result.reasons).toContain('length')
     })
 
-    it('should return null for non-string, non-object values', () => {
-      expect(base64.set(12345)).toBeNull()
-      expect(base64.set(true)).toBeNull()
+    it('should invalidate a password without lowercase letters', () => {
+      const result = security.password.validation('PASSWORD123!')
+      expect(result.isValid).toBe(false)
+      expect(result.lowercase).toBe('Password should contain at least one lowercase character.')
+      expect(result.reasons).toContain('lowercase')
+    })
+
+    it('should invalidate a password without uppercase letters', () => {
+      const result = security.password.validation('password123!')
+      expect(result.isValid).toBe(false)
+      expect(result.uppercase).toBe('Password should contain at least one uppercase character.')
+      expect(result.reasons).toContain('uppercase')
+    })
+
+    it('should invalidate a password without digits', () => {
+      const result = security.password.validation('Password!')
+      expect(result.isValid).toBe(false)
+      expect(result.digit).toBe('Password should contain at least one digit.')
+      expect(result.reasons).toContain('digit')
+    })
+
+    it('should invalidate a password without special characters', () => {
+      const result = security.password.validation('Password123')
+      expect(result.isValid).toBe(false)
+      expect(result.special).toBe('Password should contain at least one special character.')
+      expect(result.reasons).toContain('special')
+    })
+
+    it('should accumulate multiple validation reasons', () => {
+      const result = security.password.validation('PASSWORD')
+      expect(result.isValid).toBe(false)
+      expect(result.reasons).toContain('digit')
+      expect(result.reasons).toContain('special')
+    })
+
+    it('should match two strong identical passwords', () => {
+      expect(security.password.match('StrongPass123!', 'StrongPass123!')).toBe(true)
+    })
+
+    it('should not match two different passwords', () => {
+      expect(security.password.match('StrongPass123!', 'DifferentPass456!')).toBe(false)
+    })
+
+    it('should not match if one of the passwords is weak', () => {
+      expect(security.password.match('StrongPass123!', 'weakpass')).toBe(false)
+    })
+
+    it('should not match if both passwords are weak', () => {
+      expect(security.password.match('weakpass1', 'weakpass1')).toBe(false)
+    })
+
+    it('encrypts password correctly', () => {
+      const encrypted = security.password.encrypt('password')
+      expect(encrypted).toHaveLength(40)
+    })
+  })
+
+  describe('csp', () => {
+    it('generates csp string', () => {
+      const cspConfig = {
+        'default-src': ["'self'"],
+        'script-src': ["'self'", 'example.com']
+      }
+      expect(security.csp.generator(cspConfig)).toBe(
+        "default-src 'self'; script-src 'self' example.com"
+      )
+    })
+  })
+
+  describe('mask', () => {
+    it('masks email correctly', () => {
+      expect(security.mask.email('test@example.com')).toMatch(/^tes\*+@ex\*+\.com$/)
+    })
+
+    it('masks phone correctly', () => {
+      expect(security.mask.phone('1234567890')).toBe('xxxxx67890')
+    })
+
+    it('masks text correctly', () => {
+      expect(security.mask.text('testingtesting', 2, 2)).toBe('te**********ng')
     })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,184 @@
 import isJSON from '@architecturex/utils.isjson'
+import crypto from 'crypto'
 
-const base64 = {
-  get(value: any) {
-    let buffer = ''
+type PasswordOptions = {
+  length?: number
+  lowercase?: boolean
+  uppercase?: boolean
+  digit?: boolean
+  special?: boolean
+}
 
-    if (typeof value === 'string') {
-      buffer = Buffer.from(value, 'base64').toString('ascii')
+type ValidationResult = {
+  isValid: boolean
+  length?: string
+  lowercase?: string
+  uppercase?: string
+  digit?: string
+  special?: string
+  reasons?: string[]
+}
+
+type CspDirective =
+  | 'default-src'
+  | 'script-src'
+  | 'style-src'
+  | 'img-src'
+  | 'connect-src'
+  | 'font-src'
+  | 'object-src'
+  | 'media-src'
+  | 'frame-src'
+  | 'sandbox'
+  | 'report-uri'
+  | 'child-src'
+  | 'form-action'
+  | 'frame-ancestors'
+  | 'plugin-types'
+  | 'worker-src'
+  | 'manifest-src'
+  | 'navigate-to'
+
+type CspConfig = {
+  [directive in CspDirective]?: string[]
+}
+
+const security = {
+  string: {
+    code(length: number): string {
+      const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+      let result = ''
+
+      for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length))
+      }
+
+      return result
+    },
+    sanitize(input: string): string {
+      return input
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;')
     }
-
-    if (isJSON(buffer)) {
-      buffer = JSON.parse(Buffer.from(value, 'base64').toString('ascii'))
-    }
-
-    return buffer
   },
-  set(value: any) {
-    if (value && typeof value === 'object') {
-      return Buffer.from(JSON.stringify(value)).toString('base64')
-    }
+  base64: {
+    encode(value: any) {
+      if (value && typeof value === 'object') {
+        return Buffer.from(JSON.stringify(value)).toString('base64')
+      }
 
-    return typeof value === 'string' ? Buffer.from(value).toString('base64') : null
+      return typeof value === 'string' ? Buffer.from(value).toString('base64') : null
+    },
+    decode(value: any) {
+      let buffer = ''
+
+      if (typeof value === 'string') {
+        buffer = Buffer.from(value, 'base64').toString('ascii')
+      }
+
+      if (isJSON(buffer)) {
+        buffer = JSON.parse(Buffer.from(value, 'base64').toString('ascii'))
+      }
+
+      return buffer
+    }
+  },
+  password: {
+    validation(password: string, options: PasswordOptions = {}): ValidationResult {
+      const validations = {
+        length: options.length || 8,
+        lowercase: options.lowercase || true,
+        uppercase: options.uppercase || true,
+        digit: options.digit || true,
+        special: options.special || true,
+        reasons: []
+      }
+
+      const errors: ValidationResult = {
+        isValid: true,
+        reasons: []
+      }
+
+      // Check for password length
+      if (password.length < validations.length) {
+        errors.reasons?.push('length')
+        errors.length = `Password should be at least ${validations.length} characters long.`
+        errors.isValid = false
+      }
+
+      // Check for at least one lowercase character
+      if (validations.lowercase && !/[a-z]/.test(password)) {
+        errors.reasons?.push('lowercase')
+        errors.lowercase = 'Password should contain at least one lowercase character.'
+        errors.isValid = false
+      }
+
+      // Check for at least one uppercase character
+      if (validations.uppercase && !/[A-Z]/.test(password)) {
+        errors.reasons?.push('uppercase')
+        errors.uppercase = 'Password should contain at least one uppercase character.'
+        errors.isValid = false
+      }
+
+      // Check for at least one digit
+      if (validations.digit && !/[0-9]/.test(password)) {
+        errors.reasons?.push('digit')
+        errors.digit = 'Password should contain at least one digit.'
+        errors.isValid = false
+      }
+
+      // Check for at least one special character
+      if (validations.special && !/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+        errors.reasons?.push('special')
+        errors.special = 'Password should contain at least one special character.'
+        errors.isValid = false
+      }
+
+      return errors
+    },
+    match(p1: string, p2: string): boolean {
+      return (
+        security.password.validation(p1).isValid &&
+        security.password.validation(p2).isValid &&
+        p1 === p2
+      )
+    },
+    encrypt(str: string): string {
+      return crypto.createHash('sha1').update(str.toString()).digest('hex')
+    }
+  },
+  csp: {
+    generator(config: CspConfig): string {
+      return Object.entries(config)
+        .map(([directive, sources]) => {
+          return `${directive} ${sources?.join(' ')}`
+        })
+        .join('; ')
+    }
+  },
+  mask: {
+    email(email: string): string {
+      const [localPart, domainPart] = email.split('@')
+      const [domainName, tld] = domainPart.split('.')
+      return `${localPart.substring(0, 3)}*****@${domainName.substring(0, 2)}*****.${tld}`
+    },
+    phone(phone: string): string {
+      const visibleDigits = 5
+      const masked = phone.slice(0, -visibleDigits).replace(/\d/g, 'x')
+      return `${masked}${phone.slice(-visibleDigits)}`
+    },
+    text(data: string, startKeep: number = 2, endKeep: number = 2, maskChar: string = '*'): string {
+      if (data.length <= startKeep + endKeep) return data
+      const visibleStart = data.slice(0, startKeep)
+      const visibleEnd = data.slice(-endKeep)
+      const maskLength = data.length - (startKeep + endKeep)
+      const maskedPart = Array(maskLength).fill(maskChar).join('')
+      return `${visibleStart}${maskedPart}${visibleEnd}`
+    }
   }
 }
 
-export default base64
+export default security
